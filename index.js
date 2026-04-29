e84625146ccb24bfa7a9d5f6750559d92502d05d
soham <sohamsatyajitpatil@gmail.com>
2026-04-29 12:59:23 +0530

    abc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Demo App Running 🚀");
+  res.send("DevOps Demo App Running...first 🚀");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
